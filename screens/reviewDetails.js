369b42dd1bac9a89827a8a5f206dbe913a91774d
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-15 00:31:18 +0530

    Starting the Reviews App

---
+++ screens/reviewDetails.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function RevieDetails() {
+    return (
+        <View style={styles.container}>
+            <Text>Review Details Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 24,
+    },
+  });
+  
