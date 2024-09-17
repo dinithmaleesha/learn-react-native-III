ca94d0194ebe625fbd8b6d2dbc35e1817845bd1b
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-17 19:27:41 +0530

    Custom Card Component

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from '../styles/global';
+import Card from '../shared/card'
 
 export default function ReviewDetails({ route }) {
   const { title, body, rating } = route.params;
 
   return (
     <View style={globalStyles.container}>
-      <Text>{title}</Text>
-      <Text>{body}</Text>
-      <Text>{rating}</Text>
+      <Card>
+        <Text>{title}</Text>
+        <Text>{body}</Text>
+        <Text>{rating}</Text>
+      </Card>
     </View>
   )
 }
