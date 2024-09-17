3e3848600dc3b875f7aa822bb89093d90acbeb89
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-17 23:58:10 +0530

    Wrap Up

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -46,6 +46,7 @@ export default function ReviewForm({ addReview }) {
             </Text>
             <TextInput
               multiline
+              minHeight={60}
               style={globalStyles.input}
               placeholder='Review body'
               onChangeText={props.handleChange('body')}
