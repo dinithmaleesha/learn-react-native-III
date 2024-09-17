b6555479f0ff07a79a31c78d1a29e50cc6457edc
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-17 22:41:15 +0530

    Background Images

--- shared/header.js
+++ shared/header.js
@@ -1,39 +1,58 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react'
+import { View, StyleSheet, Text, Dimensions, Image, ImageBackground } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header({ navigation, title }) {
-    const openMenu = () => {
-        navigation.openDrawer();
-    };
+  const openMenu = () => {
+    navigation.openDrawer();
+  }
 
-    return (
-        <View style={styles.header}>
-            <MaterialIcons name="menu" size={28} onPress={openMenu} style={styles.icon} />
-            <Text style={styles.headerText} numberOfLines={1}>
-                {title}
-            </Text>
-        </View>
-    );
+  return (
+    <ImageBackground style={styles.header} source={require('../assets/game_bg.png')} >
+      <MaterialIcons name="menu" size={28} onPress={openMenu} style={styles.icon} />
+      <View style={styles.headerTitle}>
+        <Image style={styles.headerImage} source={require('../assets/heart_logo.png')} />
+        <Text style={styles.headerText}>
+          {title}
+        </Text>
+      </View>
+    </ImageBackground>
+  )
 }
 
+
 const styles = StyleSheet.create({
-    header: {
-        width: '100%',
-        height: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    headerText: {
-        fontWeight: 'bold',
-        fontSize: 20,
-        color: '#333',
-        letterSpacing: 1,
-        textAlign: 'center',
-    },
-    icon: {
-        position: 'absolute',
-        left: 10,
-    },
+  header: {
+    width: Dimensions.get('screen').width,
+    height: '100%',
+    marginTop: -25,
+    marginLeft: -15,
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#333',
+    marginTop: 15,
+    letterSpacing: 1,
+    height: '100%',
+    alignItems: "center",
+  },
+  icon: {
+    position: 'relative',
+    flexDirection: 'column',
+    flex: 0.5,
+    marginTop: 15,
+  },
+  headerImage: {
+    width: 26,
+    height: 26,
+    marginHorizontal: 10,
+    marginTop: 15,
+  },
+  headerTitle: {
+    flexDirection: 'row'
+  }
 });
